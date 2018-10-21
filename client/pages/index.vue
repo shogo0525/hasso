495a4f9f0fc0759c6216383458ce781606fa4f9e
shogo_inamoto <shogo.ca2011@gmail.com>
2018-10-21 12:58:19 +0900

    Implement Nuxt.js to Laravel

---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div> {{data}} </div>
+</template>
+<script>
+export default{
+  async asyncData({app}){
+    const data = await app.$axios.$get('http://localhost:8000/api/')
+    return {data};
+  }
+}
+</script>
